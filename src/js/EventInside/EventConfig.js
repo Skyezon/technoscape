ed7cf770703b42bd3bb31b156b22cbc5f8f6f3b2
Delbert <richarddelbert84@gmail.com>
2020-07-21 16:52:52 +0700

    footer done

--- src/js/EventInside/EventConfig.js
+++ src/js/EventInside/EventConfig.js
@@ -6,7 +6,7 @@ const events = [
     {
         normalHeader: "Virtual",
         'wierdHeader' : "Conference",
-        "time" : "22 - 24 August 2020",
+        "time" : `11 <sup>th &nbsp;</sup> - &nbsp;13 <sup>th &nbsp;</sup>September 2020`,
         "paragraph" : [
             `Virtual Conference is a 3-day Webinar & Workshop series that will educate participants regarding <b>business</b>, <b>design</b>, and <b>technology</b>.`,
             "Participants will gain insights and skills which they can implement to solve problems in real-life and have a chance to interact with experts from the field.",
@@ -18,7 +18,7 @@ const events = [
     {
         'normalHeader' : "Road to",
         'wierdHeader' : "Hackathon",
-        "time" : "Once a week in September 2020 ",
+        "time" : `18<sup>th &nbsp;</sup>- &nbsp;30<sup>th &nbsp;</sup> September 2020 (2x per week)`,
         "paragraph" : [
             `Road to BNCC Hackathon is a <b>weekly webinar series</b> which will talk about topics that are relevant in the IT field from notable speakers.`,
             `Road to Hackathon exists as a forum for people who want to develop their knowledge in technology and to interact with fellow technology enthusiasts.`
@@ -27,9 +27,9 @@ const events = [
         "image" : RthImage
     },
     {
-        'normalHeader' : `<img src="${LogoHack}" class="d-block" alt="logo">`,
-        'wierdHeader' : "Hackathon 3.0",
-        "time" : "10 - 11 October 2020",
+        'normalHeader' : `<img src="${LogoHack}" class="d-block" alt="logo"> Hackathon `,
+        'wierdHeader' : "3.0",
+        "time" : `10<sup>th &nbsp;</sup>- &nbsp;11<sup>th &nbsp;</sup> October 2020`,
         "paragraph" : [
             `Hackathon is a 24 hour coding competition which challenges its participants to create innovative applications or websites to solve real life problems. `,
             `Participants will have a chance to interact and gain insight from mentors in the business, technology, and design mentoring session.`
--- src/js/EventInside/EventGenerator.js
+++ src/js/EventInside/EventGenerator.js
@@ -19,15 +19,15 @@ class EventGenerator {
         eventData.forEach((data,index) => {
             const {normalHeader,wierdHeader,time,paragraph,link,image} = data;
             eventInside.innerHTML +=`
-            <div class="row position-relative event-row py-5 d-flex" id="event-inside-${index}">
-                <div class="col-lg-6 pl-5">
+            <div class="row position-relative event-row py-5 px-lg-5 d-flex" id="event-inside-${index}">
+                <div class="col-lg-6 pl-lg-5 ">
                     <div class="font-headline-1 position-relative d-flex flex-column wow" id="event-heading-${index}">
                         <span> ${normalHeader} </span>
                         <span class="font-shadowline" id="shadowline-${index}"><span class="font-shadowline-before"></span>${wierdHeader}<span class="font-shadowline-after"></span></span>
                     </div>
                     <div class="font-caption my-3 d-flex align-items-center wow" id="timing-${index}"><img src="${DateIcon}" alt="date-icon" class="mr-3">${time}</div>
-                    <div id="event-paragraph-${index}" class="wow"></div>
-                    <div class="position-relative d-flex">
+                    <div id="event-paragraph-${index}" class="wow text-justify"></div>
+                    <div class="position-relative d-flex wow" id="external-link-${index}">
                         <a class="my-5 d-flex align-items-center event-link font-button" href="${link}">Visit the web <img src="${ExternalLink}" class="ml-3" alt="external-link-icon" /> </div></a>
                     </div>
                     <div class="col-lg-6 position-relative  d-flex justify-content-center wow" id="event-image-container-${index}">
@@ -37,26 +37,28 @@ class EventGenerator {
                 </div>
             </div>
         `
-            const fontWierd = document.getElementById(`shadowline-${index}`)
-            $(`#shadowline-${index} > span`).text(fontWierd.innerText)
-            $(`#shadowline-${index} > p`).text(fontWierd.innerText)
+            const fontWierd = $(`#shadowline-${index}`)
+            $(`#shadowline-${index} span`).text(fontWierd.text())
            const eventHeading = document.getElementById(`event-heading-${index}`)
             const eventImage = document.getElementById(`event-image-container-${index}`)
             const eventParagraph = document.getElementById(`event-paragraph-${index}`)
             const eventTime = document.getElementById(`timing-${index}`)
+            const eventLink = document.getElementById(`external-link-${index}`)
 
             if (index % 2 != 0){
                 document.getElementById(`event-inside-${index}`).classList.add('flex-row-reverse')
                 document.getElementById(`mesh-${index}`).style.right = '22rem'
-                eventHeading.classList.add('animate__rotateInDownRight')
+                eventHeading.classList.add('animate__fadeInRight')
                 eventImage.classList.add('animate__fadeInLeft')
                 eventParagraph.classList.add('animate__fadeInRight')
                 eventTime.classList.add('animate__fadeInRight')
+                eventLink.classList.add('animate__fadeInRight')
             }else{
-                eventHeading.classList.add('animate__rotateInDownLeft')
+                eventHeading.classList.add('animate__fadeInLeft')
                 eventImage.classList.add('animate__fadeInRight')
                 eventParagraph.classList.add('animate__fadeInLeft')
                 eventTime.classList.add('animate__fadeInLeft')
+                eventLink.classList.add('animate__fadeInLeft')
 
             }
             paragraph.forEach((data) => {
